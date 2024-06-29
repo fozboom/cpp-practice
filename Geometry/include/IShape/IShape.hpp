d568f3c1aa408b4b2c42004c68bc979f09599e90
fozboom <fozboom@gmail.com>
2024-06-29 14:40:51 +0300

    Ishape: create abstract class

---
+++ Geometry/include/IShape/IShape.hpp
@@ -0,0 +1,12 @@
+#ifndef GEOMETRY_INCLUDE_ISHAPE_ISHAPE_HPP_
+#define GEOMETRY_INCLUDE_ISHAPE_ISHAPE_HPP_
+
+class IShape {
+ public:
+  IShape() = default;
+
+
+  virtual ~IShape() = default;
+};
+
+#endif  // GEOMETRY_INCLUDE_ISHAPE_ISHAPE_HPP_
